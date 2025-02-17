759e5e0c6beda1d0db8263d7f8ef1fb1e327d117
Lucio Benfante <lucio@benfante.com>
2025-02-17 15:40:24 +0100

    Added i18n and base header layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,10 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    locales: ["it", "en"],
+    defaultLocale: "it",
+  },
   vite: {
     plugins: [tailwindcss()]
   },
