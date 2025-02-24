cf97dc94e09af03d2a4a67d7f273f16c93c41429
Lucio Benfante <lucio@benfante.com>
2025-02-24 19:05:27 +0100

    Add social media icons to footer and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import paraglide from "@inlang/paraglide-astro";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   i18n: {
@@ -21,10 +23,8 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react(),
-    paraglide({
-      project: "./project.inlang",
-      outdir: "./src/libs/paraglide",
-    }),
-  ],
+  integrations: [react(), paraglide({
+    project: "./project.inlang",
+    outdir: "./src/libs/paraglide",
+  }), icon()],
 });
